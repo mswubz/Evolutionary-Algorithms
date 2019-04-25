77886c0e7607e5627c6598aec986199f2a36711b
Jocelyn Ragukonis <jnragu@gmail.com>
2019-04-24 21:27:13 -0400

    working on run file

--- createIndividuals.py
+++ createIndividuals.py
@@ -2,14 +2,9 @@ import random
 
 class Individuals(object):
 
-    def __init__(self, length, sizePopulation, password):
-        self.__length = length
+    def __init__(self, sizePopulation, password):
         self.__sizePopulation = sizePopulation
         self.__password = password
-
-    @property
-    def length(self):
-        return self.__length
 
     @property
     def sizePopulation(self):
@@ -19,15 +14,7 @@ class Individuals(object):
     def password(self):
         return self.__password
 
-    def generateWord (length):
-        ''' Generates a random word of input length '''
-        i = 0
-        result = ""
-        while i < length:
-            letter = chr(97 + int(26 * random.random()))
-            result += letter
-            i +=1
-        return result
+
 
     def generateFirstPopulation(sizePopulation, password):
         ''' Generates the first population with input size and password '''
--- run.py
+++ run.py
@@ -4,8 +4,42 @@ from createIndividuals import Individuals
 from breederReproduction import Reproduction
 from mutation import Mutation
 
-print("Welcome to the Knapsack Problem")
-print("First we need to generate a word")
-print("Please enter an integer for the length of word")
+import random
 
-length = input()
+def generateWord (length):
+    ''' Generates a random word of input length '''
+    i = 0
+    result = ""
+    while i < length:
+        letter = chr(97 + int(26 * random.random()))
+        result += letter
+        i +=1
+    return result
+
+
+print("Welcome to the Knapsack Problem")
+
+print("Enter an integer for the length of password")
+password_length = int(input())
+
+password = generateWord(password_length)
+
+print("Password is: ")
+print(password)
+
+print("Enter an integer for the size of population")
+size_Population = int(input())
+
+Individuals = Individuals(password, size_Population)
+
+print("The first population is: ")
+first_population = Individuals.generateFirstPopulation(size_Population, password)
+print(first_population)
+
+#Fitness = Fitness()
+#Breeders = Breeders()
+#Reproduction = Reproduction()
+#Mutation = Mutation()
+
+
+
